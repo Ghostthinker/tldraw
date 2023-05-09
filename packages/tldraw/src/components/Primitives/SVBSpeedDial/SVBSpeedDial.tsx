fcd380e6991daeedbd4e304acca909f992e4a439
manu <manuel.klapfenberger@ghostthinker.de>
2023-05-09 09:54:27 +0200

    speeddial base ready, will update tldraw now

--- packages/tldraw/src/components/Primitives/SVBSpeedDial/SVBSpeedDial.tsx
+++ packages/tldraw/src/components/Primitives/SVBSpeedDial/SVBSpeedDial.tsx
@@ -34,7 +34,11 @@ function handleSpeedDialClick(action: string) {
 const dialItems = [
   {
     label: 'Add content from edubreak',
-    icon: PrimeIcons.PLUS,
+    icon: <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+      '<path className={'eb-icon-out'} d="M12.2917 0.132217C12.8583 -0.182711 13.4974 0.0727725 13.5 0.815192V10.2112C13.5 11.1977 13.2602 11.6568 12.5012 12.1273L2.15188 17.8618C1.58263 18.1767 0.90269 17.9516 0.900055 17.2066V7.81063C0.897419 7.06821 1.06477 6.35615 1.89888 5.89451L12.2917 0.132217Z" fill="white"/>\n' +
+      '<path className={'eb-icon-out'} d="M15.9696 2.76744L13.5 3.66986V10.8L16.0299 9.88642C16.6245 9.67321 17.1 9.30505 17.1 8.55138V3.4269C17.1 2.86165 16.563 2.55547 15.9684 2.76744" fill="white"/>\n' +
+      '<path className={'eb-icon-in'} d="M13.5 3.60001L4.75137 6.59504C4.11007 6.81407 3.59455 7.42785 3.60008 7.96777V13.3313C3.60423 13.87 4.17641 14.1298 4.81771 13.9107L5.67875 13.6153V15.3L7.8749 12.864L13.4987 10.9373V3.60001H13.5Z" fill="#023047"/>\n' +
+      '</svg>,
     command: () => {
       handleSpeedDialClick(ADD_FROM_EDUBREAK)
     }
@@ -87,6 +91,34 @@ const StyledSpeedDial = styled(SpeedDial)`
   .p-speeddial-action {
     background: transparent;
     border: 3px solid #023047;
+  }
+
+  .p-speeddial-action:hover {
+    background: #023047;
+
+    .pi {
+      color: white;
+    }
+
+    .eb-icon-out {
+      fill: white;
+    }
+
+    .eb-icon-in {
+      fill: #023047;
+    }
+  }
+
+  .pi {
+    color: #023047;
+  }
+
+  .eb-icon-out {
+    fill: #023047;
+  }
+
+  .eb-icon-in {
+    fill: white;
   }
 `;
 
