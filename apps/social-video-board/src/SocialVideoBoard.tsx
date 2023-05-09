401c6f3f97923626ed432bb9e50c87906a2df85c
manu <manuel.klapfenberger@ghostthinker.de>
2023-05-09 12:51:19 +0200

    social-video-board tldraw version created

---
+++ apps/social-video-board/src/SocialVideoBoard.tsx
@@ -0,0 +1,13 @@
+import { Tldraw } from '@tldraw/tldraw'
+// eslint-disable-next-line import/no-internal-modules
+import '@tldraw/tldraw/editor.css'
+// eslint-disable-next-line import/no-internal-modules
+import '@tldraw/tldraw/ui.css'
+
+export default function SocialVideoBoard() {
+	return (
+		<div className="tldraw__editor">
+			<Tldraw autoFocus />
+		</div>
+	)
+}
---
+++ apps/social-video-board/src/index.tsx
@@ -0,0 +1,45 @@
+// eslint-disable-next-line import/no-internal-modules
+import { getBundlerAssetUrls } from '@tldraw/assets/src/urls'
+import {
+	DefaultErrorFallback,
+	ErrorBoundary,
+	setDefaultEditorAssetUrls,
+	setDefaultUiAssetUrls,
+} from '@tldraw/tldraw'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SocialVideoBoard from './SocialVideoBoard'
+
+
+// we use secret internal `setDefaultAssetUrls` functions to set these at the
+// top-level so assets don't need to be passed down in every single example.
+const assetUrls = getBundlerAssetUrls()
+setDefaultEditorAssetUrls(assetUrls)
+setDefaultUiAssetUrls(assetUrls)
+
+type SocialVideoBoard = {
+	path: string
+	element: JSX.Element
+}
+export const allExamples: SocialVideoBoard[] = [
+	{
+		path: '/',
+		element: <SocialVideoBoard />,
+	}
+]
+
+const router = createBrowserRouter(allExamples)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement!)
+
+root.render(
+	<StrictMode>
+		<ErrorBoundary
+			fallback={(error) => <DefaultErrorFallback error={error} />}
+			onError={(error) => console.error(error)}
+		>
+			<RouterProvider router={router} />
+		</ErrorBoundary>
+	</StrictMode>
+)
