c52fca2f2a17f8fe9b9e5effb8526e1245d5d129
manu <manuel.klapfenberger@ghostthinker.de>
2023-05-22 12:47:50 +0200

    slimmed down page menu

--- packages/ui/src/lib/hooks/useMenuSchema.tsx
+++ packages/ui/src/lib/hooks/useMenuSchema.tsx
@@ -76,6 +76,149 @@ export function MenuSchemaProvider({ overrides, children }: MenuSchemaProviderPr
 	const canRedo = useCanRedo()
 	const isZoomedTo100 = useValue('isZoomedTo100', () => app.zoomLevel === 1, [app])
 
+	const edubreakMenuSchema = useMemo<MenuSchema>(() => {
+		const menuSchema = compact([
+			menuGroup(
+				'menu',
+				// menuSubmenu(
+				// 	'file',
+				// 	'menu.file',
+				// 	menuGroup('print', menuItem(actions['print'], { disabled: emptyPage }))
+				// ),
+				menuSubmenu(
+					'edit',
+					'menu.edit',
+					menuGroup(
+						'undo-actions',
+						menuItem(actions['undo'], { disabled: !canUndo }),
+						menuItem(actions['redo'], { disabled: !canRedo })
+					),
+					menuGroup(
+						'clipboard-actions',
+						menuItem(actions['cut'], { disabled: noneSelected }),
+						menuItem(actions['copy'], { disabled: noneSelected }),
+						{
+							id: 'MENU_PASTE',
+							type: 'custom',
+							disabled: !showMenuPaste,
+							readonlyOk: false,
+						}
+					),
+					// menuGroup(
+					// 	'conversions',
+					// 	menuSubmenu(
+					// 		'copy-as',
+					// 		'menu.copy-as',
+					// 		menuGroup(
+					// 			'copy-as-group',
+					// 			menuItem(actions['copy-as-svg'], { disabled: emptyPage }),
+					// 			menuItem(actions['copy-as-png'], { disabled: emptyPage || !hasClipboardWrite }),
+					// 			menuItem(actions['copy-as-json'], { disabled: emptyPage })
+					// 		),
+					// 		menuGroup(
+					// 			'export-bg',
+					// 			menuItem(actions['toggle-transparent'], { checked: !exportBackground })
+					// 		)
+					// 	),
+					// 	menuSubmenu(
+					// 		'export-as',
+					// 		'menu.export-as',
+					// 		menuGroup(
+					// 			'export-as-group',
+					// 			menuItem(actions['export-as-svg'], { disabled: emptyPage }),
+					// 			menuItem(actions['export-as-png'], { disabled: emptyPage }),
+					// 			menuItem(actions['export-as-json'], { disabled: emptyPage })
+					// 		),
+					// 		menuGroup(
+					// 			'export-bg',
+					// 			menuItem(actions['toggle-transparent'], { checked: !exportBackground })
+					// 		)
+					// 	)
+					// ),
+					menuGroup(
+						'set-selection-group',
+						menuItem(actions['select-all'], { disabled: emptyPage }),
+						menuItem(actions['select-none'], { disabled: !oneSelected })
+					),
+					menuGroup(
+						'selection',
+						showAutoSizeToggle && menuItem(actions['toggle-auto-size']),
+						showEditLink && menuItem(actions['edit-link']),
+						menuItem(actions['duplicate'], { disabled: !oneSelected }),
+						allowGroup && menuItem(actions['group']),
+						allowUngroup && menuItem(actions['ungroup'])
+					),
+					menuGroup('delete-group', menuItem(actions['delete'], { disabled: !oneSelected }))
+				),
+				menuSubmenu(
+					'view',
+					'menu.view',
+					menuGroup(
+						'view-actions',
+						menuItem(actions['zoom-in']),
+						menuItem(actions['zoom-out']),
+						menuItem(actions['zoom-to-100'], { disabled: isZoomedTo100 }),
+						menuItem(actions['zoom-to-fit'], { disabled: emptyPage }),
+						menuItem(actions['zoom-to-selection'], { disabled: emptyPage || !oneSelected })
+					)
+				)
+			),
+			menuGroup('extras', menuItem(actions['insert-embed']), menuItem(actions['insert-media'])),
+			menuGroup(
+				'preferences',
+				menuSubmenu(
+					'preferences',
+					'menu.preferences',
+					menuGroup(
+						'preferences-actions',
+						menuItem(actions['toggle-snap-mode'], { checked: isSnapMode }),
+						menuItem(actions['toggle-tool-lock'], { checked: isToolLock }),
+						menuItem(actions['toggle-grid'], { checked: isGridMode }),
+						menuItem(actions['toggle-dark-mode'], { checked: isDarkMode }),
+						menuItem(actions['toggle-focus-mode'], { checked: isFocusMode }),
+						menuItem(actions['toggle-debug-mode'], { checked: isDebugMode })
+					)
+				),
+				isMobile && menuCustom('LANGUAGE_MENU', { readonlyOk: true })
+			),
+		])
+
+		if (overrides) {
+			return overrides(app, menuSchema, {
+				actions,
+				noneSelected,
+				oneSelected,
+				twoSelected,
+				threeSelected,
+			})
+		}
+
+		return menuSchema
+	}, [
+		app,
+		overrides,
+		actions,
+		oneSelected,
+		twoSelected,
+		threeSelected,
+		emptyPage,
+		isMobile,
+		allowGroup,
+		allowUngroup,
+		showEditLink,
+		showAutoSizeToggle,
+		noneSelected,
+		canUndo,
+		canRedo,
+		isDarkMode,
+		isGridMode,
+		isSnapMode,
+		isToolLock,
+		isFocusMode,
+		isDebugMode,
+		isZoomedTo100,
+	])
+
 	const menuSchema = useMemo<MenuSchema>(() => {
 		const menuSchema = compact([
 			menuGroup(
@@ -221,7 +364,9 @@ export function MenuSchemaProvider({ overrides, children }: MenuSchemaProviderPr
 		isZoomedTo100,
 	])
 
-	return <MenuSchemaContext.Provider value={menuSchema}>{children}</MenuSchemaContext.Provider>
+	return (
+		<MenuSchemaContext.Provider value={edubreakMenuSchema}>{children}</MenuSchemaContext.Provider>
+	)
 }
 
 /** @public */
